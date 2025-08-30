b569ba52b40ba691b822642c792f72d9c84e2143
Madhu Kuncham <madhukuncham3@gmail.com>
2025-08-30 11:40:34 +0530

    Initial commit -sentimental Analysis project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <button className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600">
+        Tailwind Works 🎉
+      </button>
+    </div>
+  )
+}
+
+export default App;
+
